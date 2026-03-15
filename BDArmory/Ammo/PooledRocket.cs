095ca47726dbb193d4a89a5f1e976746ac870dfb
BillNyeTheIE <BillNyeTheIlluminatiEye123@protonmail.com>
2026-03-15 14:22:37 -0700

    Efficiency
    - Combine two sqrts into one, removed unnecessary cast

--- BDArmory/Ammo/PooledRocket.cs
+++ BDArmory/Ammo/PooledRocket.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using UniLinq;
 using UnityEngine;
@@ -753,7 +753,7 @@ namespace BDArmory.Bullets
                 }
                 else
                 {
-                    bool cockpitPen = (float)(16f * impactVelocity * BDAMath.Sqrt(rocketMass) / BDAMath.Sqrt(caliber)) > Mathf.Max(20 / anglemultiplier, 1);
+                    bool cockpitPen = (16f * impactVelocity * BDAMath.Sqrt(rocketMass / caliber)) > Mathf.Max(20 / anglemultiplier, 1);
                     // Previously this was gated by cockpitPen for some reason?
                     // Seems like we should be adding ballistic damage regardless of if there's a cockpit no?
                     ProjectileUtils.ApplyDamage(hitPart, hit, dmgMult, penetrationFactor, caliber, rocketMass * 1000, impactVelocity, bulletDmgMult, distanceFromStart, explosive, incendiary, false, sourceVessel, rocketName, team, ExplosionSourceType.Rocket, penTicker <= 0, penTicker <= 0, cockpitPen);
